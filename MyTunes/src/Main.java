1f4a813fa3b798e678b29859ba5c5277f075c3d7
DonHundley <112941023+DonHundley@users.noreply.github.com>
2022-12-01 01:20:38 +0100

    All play functions
    This is the play functions in their temporary functioning state. They all work, but they are based off of an arraylist library at the moment.

--- MyTunes/src/Main.java
+++ MyTunes/src/Main.java
@@ -1,13 +1,18 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
+
         public static void main(String[] args) {
             Main.launch();
+
         }
 
         @Override
@@ -18,6 +23,20 @@ public class Main extends Application {
             primaryStage.setTitle("My Tunes");
             primaryStage.setScene(scene);
             primaryStage.show();
+
+            /**
+             * This will allow us to close the program without an error dump.
+             */
+            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    Platform.exit();
+                    System.exit(0);
+                }
+            });
+
         }
+
+
     }
 
--- MyTunes/src/gui/controller/MyTunesMainWindowController.java
+++ MyTunes/src/gui/controller/MyTunesMainWindowController.java
@@ -1,16 +1,206 @@
 package gui.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MyTunesMainWindowController {
-    public void clickSearch(ActionEvent actionEvent) {
+
+public class MyTunesMainWindowController implements Initializable {
+
+    @FXML
+    private Label songLabel;
+
+    @FXML
+    private ProgressBar songProgressBar;
+    @FXML
+    private Slider volumeSlider;
+    @FXML
+    private ComboBox<String> speedBox;
+
+    private MediaPlayer mediaPlayer;
+    private Media media;
+
+    private File directory;
+    private File[] files;
+
+    /**
+     * This is a temporary solution to make a mock library of our songs.
+     */
+    private ArrayList<File> songs;
+    private int songNumber;
+    private int[] speeds = {25, 50, 75, 100, 125, 150, 175, 200};
+
+
+    private Timer timer;
+    private TimerTask task;
+
+    private boolean running;
+
+    /**
+     * This Method is creating an arraylist I am using for a mock library until the database is complete.
+     * This Method also implements our volume slider and sets the program to the first song in the music folder.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        /**
+         * Note: I could not figure out how to make the file work by calling only for the music folder, so I had to make
+         * an absolute path reference. This is a large part of a temporary patch to make sure all methods work until we have the database.
+         */
+
+        songs = new ArrayList<File>();
+        directory = new File("C:\\Users\\Don\\Documents\\GitHub\\MyTunes\\MyTunes\\src\\gui\\music");
+        files = directory.listFiles();
+        if(files != null){
+            for(File file : files){
+                songs.add(file);
+
+            }
+        }
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        songLabel.setText(songs.get(songNumber).getName());
+
+        for(int i = 0; i < speeds.length; i++) {
+            speedBox.getItems().add(Integer.toString(speeds[i])+"%");
+        }
+
+        speedBox.setOnAction(this::changeSpeed);
+
+
+        volumeSlider.valueProperty().addListener(new ChangeListener<Number>(){
+
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+               mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
+            }
+        });
     }
+
+
+
+
+    /**
+     * This method is what is used to begin playing a song.
+     * It also starts the timer for the song progress bar, makes sure our volume always matches the volume slider
+     * and changes our speed setting when we play a new song.
+     *
+     */
+    private void playSong()
+    {
+        beginTimer();
+        changeSpeed(null);
+        mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
+        mediaPlayer.play();
+    }
+
+    /**
+     * This method stops our music and pauses the timer.
+     */
+    public void pauseMedia()
+    {
+        cancelTimer();
+        mediaPlayer.pause();
+    }
+
+    /**
+     * This method resets our song and the progress bar to the beginning.
+     */
+    public void resetMedia()
+    {
+        songProgressBar.setProgress(0);
+        mediaPlayer.seek(Duration.seconds(0));
+    }
+
+
+    /**
+     * This method determines what happens when we hit the play button.
+     * If the player is not running, it will begin playing a song. If the player is running, it will stop.
+     */
+    @FXML
+    void playMedia(ActionEvent event) {
+        if(running)
+            pauseMedia();
+        else {
+            playSong();
+        }
+    }
+
+
+    /**
+     * This method allows us to skip to the next song.
+     * The if statement is checking if we are at the end of our list or not and allowing us to loop the "playlist" infinitely.
+     */
+    @FXML
+    void nextMedia(ActionEvent event) {
+        if(songNumber < songs.size() - 1)
+        {
+            songNumber++;
+            changeMediaPlayer();
+        }
+        else
+        {
+            songNumber = 0;
+            changeMediaPlayer();
+        }
+
+    }
+
+
+    /**
+     * This method allows us to skip to the previous song.
+     * We are checking if we are at the end of our list or not and allowing us to loop the "playlist" infinitely.
+     * We also use this method to note our current progress in the song, and if we hit previous past 5 seconds it will begin the song again.
+     * If the user then hits previous a second time it will go back a song.
+     */
+    @FXML
+    void previousMedia(ActionEvent event) {
+        int i = (int) mediaPlayer.getCurrentTime().toSeconds();
+        if(songNumber > 0)
+        {
+
+            if(i > 5){
+                resetMedia();
+            }
+            else {
+
+                songNumber--;
+                mediaPlayer.stop();
+                changeMediaPlayer();
+            }
+        }
+
+        else {
+            if(i > 5){
+                resetMedia();
+            }
+            else {
+                songNumber = songs.size() - 1;
+                changeMediaPlayer();
+            }
+        }
+    }
+
 
     public void ClickNewPlaylist(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/gui/view/NewPlaylistView.fxml"));
@@ -47,4 +237,74 @@ public class MyTunesMainWindowController {
         stage.setScene(scene);
         stage.show();
     }
+
+    /**
+     * This method is what we use to determine the passed time since the song began, and how much time is left.
+     * We compare our current time and our end time to get our current progress percentage.
+     * if the progress reaches 100%, the timer is reset.
+     */
+    public void beginTimer()
+    {
+        timer = new Timer();
+
+        task = new TimerTask() {
+            public void run() {
+                running = true;
+                double current = mediaPlayer.getCurrentTime().toSeconds();
+                double end = media.getDuration().toSeconds();
+                songProgressBar.setProgress(current / end);
+
+                if (current / end == 1) {
+                    cancelTimer();
+                }
+            }
+        };
+        timer.scheduleAtFixedRate(task, 0, 1000);
+    }
+
+    /**
+     * resets our timer and lets us our program know we are no longer running a song.
+     */
+    public void cancelTimer(){
+        running = false;
+        timer.cancel();
+    }
+
+    /**
+     * This method is used to reduce repeating ourselves in our skip and previous song functions, it is the bulk of what
+     * actually changes our song.
+     * First it stops the song and resets our timer. Then it retrieves a new song changes our label to match it and finally plays the new song.
+     */
+    private void changeMediaPlayer()
+    {
+        mediaPlayer.stop();
+        if(running) {
+            cancelTimer();
+        }
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        songLabel.setText(songs.get(songNumber).getName());
+        playSong();
+    }
+
+    /**
+     * This method is what we use to change the playback speed of our song.
+     */
+    public void changeSpeed(ActionEvent event) {
+        if(speedBox.getValue() == null) {
+            mediaPlayer.setRate(1);
+        }
+        else {
+            mediaPlayer.setRate(Integer.parseInt(speedBox.getValue().substring(0, speedBox.getValue().length() - 1)) * 0.01);
+        }
+    }
+
+
+
+
+    @FXML
+    void clickSearch(ActionEvent event) {
+
+    }
 }
+
